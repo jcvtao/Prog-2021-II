10556681d88d14a7a18560ec373111cc07e9b20a
Juan Camilo <juvergarat@unal.edu.co>
2021-10-13 10:16:07 -0500

    Primer ejemplo/código del curso.

---
+++ 2021-10-13-IntroCPP/ejemplo.cpp
@@ -0,0 +1,15 @@
+// Calcular los números primos entre 1 y 100
+
+// Incluir la libreria iostream (incluye las 28840 lineas al compilar)
+#include <iostream>
+
+// Función principal
+int main(void)
+{
+  int m = 1;
+  int n = 100;
+  std::cout << "m: " << m << "\n";
+  std::cout << "n: " << n << "\n";
+    
+  return 0;
+}
