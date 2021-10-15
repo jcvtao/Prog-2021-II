466505857adadb4642e905f4d86b49e1b17cb977
Juan Camilo <juvergarat@unal.edu.co>
2021-10-15 09:02:04 -0500

    Código ejemplo del uso de variables.

---
+++ 2021-10-15-Intro/variables.cpp
@@ -0,0 +1,24 @@
+// Edición: Escribir mi programa con un editor
+// Compliación: g++ holamundo.cpp
+// Ejecución: ./a.out
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hola mundo\n";
+  int a; // wrong: initialize it
+  float x; // initialize it
+  double y = 0.0;
+  std::cout << "a = " << a << "\n";
+  std::cout << "x = " << x << "\n";
+  std::cout << "y = " << y << "\n";
+
+  return 0;
+}
+
+// variables: string, bool, int, float, double, char
+// operadores: +, -, *, /
+// funciones
+// estructuras de datos básicas
+// clases
