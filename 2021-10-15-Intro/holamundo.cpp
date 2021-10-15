86849db81c221c970297f1aa89b8593599750973
Juan Camilo <juvergarat@unal.edu.co>
2021-10-15 09:00:39 -0500

    Primer código del curso (HelloWorld).

---
+++ 2021-10-15-Intro/holamundo.cpp
@@ -0,0 +1,11 @@
+// Edición: Escribir mi programa con un editor
+// Compliación: g++ holamundo.cpp
+// Ejecución: ./a.out
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hola mundo\n";
+  return 0;
+}
