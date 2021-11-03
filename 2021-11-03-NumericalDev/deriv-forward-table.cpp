17b516e1daf952e45bceb0746545f80e83b37636
Juan Camilo <juvergarat@unal.edu.co>
2021-11-03 08:07:09 -0500

    Código para generar tabla de datos (derivación numérica).

---
+++ 2021-11-03-NumericalDev/deriv-forward-table.cpp
@@ -0,0 +1,33 @@
+/*
+f'(X) = lim h-> 0 (f(x+h)-f(x))/h
+
+f'(x, h) = (f(x+h)-f(x))/h + 0(h)
+*/
+
+#include <cmath>
+#include <iostream>
+
+double fun(double x);
+double fderiv(double x, double h);
+
+int main(int argc, char **argv)
+{
+    std::cout.precision(15); std::cout.setf(std::ios::scientific);
+    const double X = std::atof(argv[1]);
+
+    for (double h = 1.0e-1; h >= 1.0e-10; h /= 10.0)
+    {
+        std::cout << h << "\t" << std::fabs(1 - fderiv(X, h)/std::cos(X)) << "\n";
+    }
+    return 0;
+}
+
+double fun(double x)
+{
+    return std::sin(x);
+}
+
+double fderiv(double x, double h)
+{
+    return (fun(x + h) - fun(x)) / h;
+}
