c9cc13e3e0554ba071d178f259472b4f06037e50
Juan Camilo <juvergarat@unal.edu.co>
2021-11-17 07:59:57 -0500

    Cálculo de raíces con método de Newton

---
+++ 2021-11-17-RootFinding-II/newton.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+double f(double x);
+double fderiv(double x, double h);
+
+template <class fptr>
+double bisection(double xl, double xu, fptr fun, double eps, int & niter);
+template <class fptr>
+double regulafalsi(double xl, double xu, fptr fun, double eps, int & niter);
+template <class fptr>
+double newton(double x0, fptr fun, double eps, int & niter);
+
+int main(int argc, char **argv)
+{
+  std::cout.precision(16); std::cout.setf(std::ios::scientific);
+  
+  const double XL = std::atof(argv[1]);
+  const double XU = std::atof(argv[2]);
+  const double EPS = std::atof(argv[3]);
+  int counter = 0;
+  std::cout << bisection(XL, XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+  std::cout << regulafalsi(XL, XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+  std::cout << newton(XL, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
+  
+  return 0;
+}
+
+double f(double x)
+{
+  return 9.81*(68.1)*(1 - std::exp(-x*10/68.1))/x - 40;
+}
+
+template <class fptr>
+double bisection(double xl, double xu, fptr fun, double eps, int & niter)
+{
+  double xr = xl;
+  int iter = 1;
+  while(std::fabs(fun(xr)) > eps) {
+    if (fun(xr)*fun(xl) < 0) {
+      xu = xr;
+    } else {
+      xl = xr;
+    }
+    xr = (xl + xu)/2; // bisection
+    iter++;
+  }
+  niter = iter;
+  return xr;
+}
+
+
+template <class fptr>
+double regulafalsi(double xl, double xu, fptr fun, double eps, int & niter)
+{
+  double xr = xl;
+  int iter = 1;
+  while(std::fabs(fun(xr)) > eps) {
+    if (fun(xr)*fun(xl) < 0) {
+      xu = xr;
+    } else {
+      xl = xr;
+    }
+    xr = xu - (f(xu)*(xl-xu))/(f(xl) - f(xu)); // regula falsi
+    iter++;
+  }
+  niter = iter;
+  return xr;
+}
+
+// xi+1 = xi - f(xi)/f'(xi)
+template <class fptr>
+double newton(double x0, fptr fun, double eps, int & niter)
+{
+    double h = 0.00001;
+    double xr = x0;
+    int iter = 1;
+    while(std::fabs(fun(xr)) > eps)
+    {
+        double fderiv = (f(xr+h/2) - f(xr-h/2))/h;
+        xr = xr - fun(xr)/fderiv;
+        iter++;
+    }
+    niter = iter;
+    return xr;
+}
